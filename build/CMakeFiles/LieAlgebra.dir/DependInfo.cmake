50b7045c7c0514d73c8f0345cf9b2ce5884b8d45
Meghdad Hasheminasab <meghdad.hasheminasab@phantom.ai>
2024-10-07 23:01:02 -0700

    Initialize

---
+++ build/CMakeFiles/LieAlgebra.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/meghdad/Dev/LieAlgebra/src/main.cpp" "/home/meghdad/Dev/LieAlgebra/build/CMakeFiles/LieAlgebra.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../3rd_party/Eigen-3.3.7"
+  "../3rd_party/Sophus"
+  "../include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/LieAlgebra.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LieAlgebra.dir/src/main.cpp.o"
+  "LieAlgebra"
+  "LieAlgebra.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LieAlgebra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
